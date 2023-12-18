4794778522f665e2fb92f344456c6b65212d78b1
LucasApalosqui <lucas@apalosqui@gmail.com>
2023-12-18 16:11:57 -0300

    Alter text to value

--- MoneyExtension/Money.cs
+++ MoneyExtension/Money.cs
@@ -8,12 +8,12 @@ namespace MoneyExtension
             if (amount < 0)
                 return 0;
 
-            var text = amount.ToString("N2").Replace(",", "").Replace(".", "");
+            var value = amount.ToString("N2").Replace(",", "").Replace(".", "");
 
-            if (string.IsNullOrEmpty(text))
+            if (string.IsNullOrEmpty(value))
                 return 0;
 
-            int.TryParse(text, out var result);
+            int.TryParse(value, out var result);
             return result;
         }
     }
